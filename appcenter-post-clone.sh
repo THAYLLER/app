1f477fa72521b81144da76e417c762069f7a3948
thayller vilela cintra <thayllervilelacintra@yahoo.com.br>
2022-06-27 15:39:46 -0300

    up

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-yarn add react-native-camera
-yarn add jetifier
-
-yarn jetify -r
-
-cd android/
-
-./gradlew clean
-./gradlew cleanBuildCache
-
-cd ../
-
-cd ios/
-pod install
-
-pod update SDWebImageWebPCoder
+rm -rf node_modules/
+yarn
+npx jetify -r
